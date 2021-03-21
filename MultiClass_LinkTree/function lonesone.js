8bac71e4c48bca9afd24e89e39f27a3c15f3f217
Christy Cooke <cleecooke@gmail.com>
2021-03-21 01:42:16 -0600

    boot strap figured out (I think), and Form part added

---
+++ MultiClass_LinkTree/function lonesone.js
@@ -0,0 +1,36 @@
+function lonesomeInteger (ints) {
+    let single = 0;
+    let i = 0;
+
+    for (i; i < ints.length; i += 1) {
+        array.forEach(element => {
+            
+        });
+    }
+    
+
+}
+// Chad's function
+// console.log(lonesomeInteger([1,1,2,2,3]));
+
+
+// function lonesomeInteger(ints){
+//     var thisInteger;
+
+//     for(var i = 0; i < ints.length; i++){
+//         thisInteger = ints[i];
+//         console.log(thisInteger);
+
+//         ints.splice(i, 1);
+//         if(ints.includes(thisInteger)) {
+//             ints.splice(ints.indexOf(thisInteger), 1);
+//         } else {
+//             ints.splice(i, 0, thisInteger);
+//         }
+//     }
+//     return ints;
+// }
+
+// console.log(lonesomeInteger([1,1,2,2,3]));
+// console.log(lonesomeInteger([1,2,1,2,3,4,5,5]));
+// console.log(lonesomeInteger([2,2]));
